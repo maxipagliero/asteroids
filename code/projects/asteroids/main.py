9fa605821eae42c0cff55b9657adbeb5c240fcac
maxipagliero <maxipagliero@gmail.com>
2025-05-20 18:20:28 +0200

    C: Draw Player

--- code/projects/asteroids/main.py
+++ code/projects/asteroids/main.py
@@ -1,16 +1,18 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
@@ -18,8 +20,10 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = fps_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
